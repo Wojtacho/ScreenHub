ef405f3e677a2822714229a0c113fee55ea1bdaa
Wojtacho <101810086+Wojtacho@users.noreply.github.com>
2023-12-15 17:33:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import PIL
+import os
+import pyautogui
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+    if request.method == 'GET':
+        data = str(GetPixelColors())
+        return data
+    return render_template('index.html')
+
+def GetPixelColors():
+    colors = []
+    screenshot = pyautogui.screenshot()
+    screenshot.save("ss.png")
+    time.sleep(0.1)
+    img = PIL.Image.open("ss.png")
+    img = img.resize((100, 100)) # Pierwszy to X drugi to Y
+    pixels = img.load()
+    
+    for y in range(100):
+        for x in range(100):
+            color = []
+            color.insert(0, pixels[x, y][0])
+            color.insert(1, pixels[x, y][1])
+            color.insert(2, pixels[x, y][2])
+            colors.insert(len(colors)-1, color)
+    os.remove("ss.png")
+    return colors
+
+#print(GetPixelColors())
+app.run(host='0.0.0.0',port='1111',debug=False)
